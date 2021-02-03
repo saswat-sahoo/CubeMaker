07930dd5d96b1b45263d7207556bb1abf9e676a9
saswat-sahoo <68820601+saswat-sahoo@users.noreply.github.com>
2021-02-04 05:27:15 +0530

    CubeMakerUpload

---
+++ Cube Maker/Assets/Scripts/CenterPLacer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CenterPLacer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public CubeGen cube;
+    private float noOfCubes;
+    private float CenterPos;
+    [SerializeField] BoxCollider TouchSensor;
+    [SerializeField] Camera View;
+    private bool done;
+   
+
+    private void Update()
+    {
+        if (!done)
+        {
+            noOfCubes = cube.HowMany;
+            if (noOfCubes != 0)
+            {
+                Offset();
+                TouchSensor.size = new Vector3(noOfCubes, noOfCubes, noOfCubes);
+                done = true;
+            }
+        }
+        
+       
+    }
+
+    void Offset()
+    {
+        
+            CenterPos = (noOfCubes - 1)/2;
+       
+        
+        Vector3 offset;
+        Vector3 offsetCam;
+        offset = new Vector3(CenterPos,CenterPos,CenterPos);
+        offsetCam = new Vector3(CenterPos, CenterPos, -5 * CenterPos-5);
+        transform.position = offset;
+        View.transform.position = offsetCam;
+    }
+}
---
+++ Cube Maker/Assets/Scripts/DragRotate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragRotate : MonoBehaviour
+{
+   
+    [SerializeField]private float Sensitivity=10;
+
+    [System.Obsolete]
+    private void OnMouseDrag()
+    {
+        float rotX = Input.GetAxis("Mouse X") * Sensitivity * Mathf.Deg2Rad;
+        float roty = Input.GetAxis("Mouse Y") * Sensitivity * Mathf.Deg2Rad;
+
+       transform.RotateAround(Vector3.up, -rotX);
+       transform.RotateAround(Vector3.right, roty);
+    }
+}
+    
+
+
