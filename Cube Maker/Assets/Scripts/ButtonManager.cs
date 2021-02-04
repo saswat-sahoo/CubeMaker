f4f46d09d4f07f235cd3a72e46cd26d97dfe5cda
saswat-sahoo <68820601+saswat-sahoo@users.noreply.github.com>
2021-02-04 05:41:03 +0530

    Upload

---
+++ Cube Maker/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class ButtonManager : MonoBehaviour
+{
+    [SerializeField] CubeGen CubeGen;
+    [SerializeField] InputField UserInput;
+    [SerializeField] Button buttontmp;
+    [SerializeField] GameObject ClosingGui;
+    
+
+
+    public void Ok()
+    {
+        CubeGen.HowMany = int.Parse(UserInput.text);
+        ClosingGui.SetActive(false);
+    }
+
+    public void Redo()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Cube Maker/Assets/Scripts/CubeGen.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeGen : MonoBehaviour
+{
+    [SerializeField] GameObject cube;
+    public int HowMany;
+    [SerializeField] private GameObject Rows;
+    [SerializeField] private GameObject Slabs;
+    [SerializeField] private GameObject MainCube;
+    private bool done;
+    // Start is called before the first frame update
+   
+    private void Update()
+    {
+        if (HowMany != 0 && !done)
+        {
+            GiantCube();
+            done = true;
+        }
+
+       
+    }
+
+
+    void GiantCube()
+    {
+        for (int k = 0; k < HowMany; k++)
+        {
+            Vector3 temp = transform.position;
+            temp.x = transform.position.x + k;
+            Instantiate(cube, temp, Quaternion.identity, Rows.transform);
+
+        }
+
+
+        for (int k = 1; k < HowMany; k++)
+        {
+            Vector3 temp = Rows.transform.position;
+            temp.z = Rows.transform.position.z + k;
+            Instantiate(Rows, temp, Quaternion.identity, Slabs.transform);
+
+        }
+        Rows.transform.SetParent(Slabs.transform);
+
+        for (int k = 1; k < HowMany; k++)
+        {
+            Vector3 temp = Slabs.transform.position;
+            temp.y = Slabs.transform.position.y + k;
+            Instantiate(Slabs, temp, Quaternion.identity, MainCube.transform);
+        }
+
+        Slabs.transform.SetParent(MainCube.transform);
+    }
+    
+}
